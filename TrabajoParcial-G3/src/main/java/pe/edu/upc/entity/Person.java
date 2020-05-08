296ea1eba94c0cb0ddc2a4f7366ea7e98b7ffa7f
WIN10 <WIN10@192.168.1.61>
2020-05-08 03:23:16 -0500

    Nickolas Commit

--- TrabajoParcial-G3/src/main/java/pe/edu/upc/entity/Person.java
+++ TrabajoParcial-G3/src/main/java/pe/edu/upc/entity/Person.java
@@ -1,50 +1,98 @@
 package pe.edu.upc.entity;
 
-public class Person {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Persona")
+public class Person  implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPerson;
+	
+	
 	private String namePerson;
 	private String lastnamePerson;
 	private String mailPerson;
 	private int dniPerson;
 	private String directionPerson;
+
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Person other = (Person) obj;
+//		if (idPerson != other.idPerson)
+//			return false;
+//		return true;
+//	}
 	
 	
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public int getIdPerson() {
 		return idPerson;
 	}
+
 	public void setIdPerson(int idPerson) {
 		this.idPerson = idPerson;
 	}
+
 	public String getNamePerson() {
 		return namePerson;
 	}
+
 	public void setNamePerson(String namePerson) {
 		this.namePerson = namePerson;
 	}
+
 	public String getLastnamePerson() {
 		return lastnamePerson;
 	}
+
 	public void setLastnamePerson(String lastnamePerson) {
 		this.lastnamePerson = lastnamePerson;
 	}
+
 	public String getMailPerson() {
 		return mailPerson;
 	}
+
 	public void setMailPerson(String mailPerson) {
 		this.mailPerson = mailPerson;
 	}
+
 	public int getDniPerson() {
 		return dniPerson;
 	}
+
 	public void setDniPerson(int dniPerson) {
 		this.dniPerson = dniPerson;
 	}
+
 	public String getDirectionPerson() {
 		return directionPerson;
 	}
+
 	public void setDirectionPerson(String directionPerson) {
 		this.directionPerson = directionPerson;
 	}
-	
+
 }
--- TrabajoParcial-G3/src/main/java/pe/edu/upc/entity/Voucher.java
+++ TrabajoParcial-G3/src/main/java/pe/edu/upc/entity/Voucher.java
@@ -1,10 +1,61 @@
 package pe.edu.upc.entity;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
 
-public class Voucher {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Voucher")
+public class Voucher implements Serializable{
+	//Esta sera mi clave unica
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idVoucher;
+	
+	
 	private Date dateVoucher;
+	
+	//uno a muchos con ID Person
+	@ManyToOne
+	@JoinColumn(name = "idPerson", nullable = false)
+	private Person idPerson;
+	
+	//uno a muchos con ID IdTypepay
+	@ManyToOne
+	@JoinColumn(name = "idTypepay", nullable = false)
+	private TypePay idTypepay;
+	
+	@OneToOne
+
+    @MapsId
+
+    private Rent rent;
+	
+	
+	
+	public Person getIdPerson() {
+		return idPerson;
+	}
+	public void setIdPerson(Person idPerson) {
+		this.idPerson = idPerson;
+	}
+	public TypePay getIdTypepay() {
+		return idTypepay;
+	}
+	public void setIdTypepay(TypePay idTypepay) {
+		this.idTypepay = idTypepay;
+	}
 	public int getIdVoucher() {
 		return idVoucher;
 	}
@@ -17,6 +68,4 @@ public class Voucher {
 	public void setDateVoucher(Date dateVoucher) {
 		this.dateVoucher = dateVoucher;
 	}
-
-	
 }
